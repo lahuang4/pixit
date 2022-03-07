99332f7777e6b4a62ea9cb8edd057a2dc0fb78fc
Lauren Huang <lahuang4@gmail.com>
2022-03-07 17:26:32 -0500

    Add support for applying a limited palette; add examples to README.

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 An image pixelizer in the browser. Play with it at: https://lahuang4.github.io/pixit/
 
-All client-side JavaScript. Upload an image to pixelize. Adjust the pixel size for more or less pixelization.
+All client-side JavaScript. Upload an image to pixelize. Adjust the pixel size for more or less pixelization. Apply a limited palette of colors to the result for more artistic experimentation, if desired.
+
+Original:
+
+![original image of a giraffe](examples/giraffe_original.png)
+
+Pixelized:
+
+![pixelized image of a giraffe](examples/giraffe_pixelized.png)
+
+With a limited palette:
+
+![limited palette image of a giraffe](examples/giraffe_limited_palette.png)
--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-function loadImage() {
+function loadImageWithoutPalette() {
+  loadImage([]);
+}
+
+function loadImage(palette) {
   var reader = new FileReader();
   reader.onload = function(e) {
     var image = new Image();
@@ -11,11 +15,11 @@ function loadImage() {
       canvas.width = w;
       canvas.height = h;
       ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, w, h);
-      paintImage(processImage());
+      paintImage(processImage(palette));
     }
     image.src = e.target.result;
   }
-  reader.readAsDataURL(document.getElementById('inputImage').files[0]);
+  reader.readAsDataURL(document.getElementById('input-image').files[0]);
   $('#toolbar').show();
 }
 
@@ -34,9 +38,9 @@ function getFitDimensions(w, h) {
   return [containerWidth, h * containerWidth / w];
 }
 
-function processImage() {
+function processImage(palette) {
   var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  imageData = pixelate(imageData, getPixelSize());
+  imageData = pixelate(imageData, getPixelSize(), palette);
   return imageData;
 }
 
@@ -48,19 +52,19 @@ function paintImage(image) {
 // Inputs
 //
 
-$('#toolbarForm').submit(function() {
+$('#toolbar-form').submit(function() {
   loadImage();
   return false;
 });
 
 function getPixelSize() {
   var pixelSize = getPixelSizeValue();
-  document.getElementById('pixelSize').value = pixelSize;
+  document.getElementById('pixel-size').value = pixelSize;
   return pixelSize;
 }
 
 function getPixelSizeValue() {
-  var pixelSize = parseInt(document.getElementById('pixelSize').value);
+  var pixelSize = parseInt(document.getElementById('pixel-size').value);
   if (!pixelSize || pixelSize < 1) {
     return 1;
   }
@@ -95,15 +99,16 @@ function copyPixel(imageData, coords, input) {
 // Image processors
 //
 
-function pixelate(imageData, pixelSize) {
+function pixelate(imageData, pixelSize, palette) {
   var processedImageData = ctx.createImageData(canvas.width, canvas.height);
   for (var x = 0; x < canvas.width; x += pixelSize) {
     for (var y = 0; y < canvas.height; y += pixelSize) {
       // Calculate avg pixel color
       var avg = getAverageColor(imageData, x, y, pixelSize, pixelSize);
+      var color = getClosestColor(avg, palette);
       for (var nx = x; nx < x + pixelSize; nx++) {
         for (var ny = y; ny < y + pixelSize; ny++) {
-          copyPixel(processedImageData, get1dCoords(imageData, nx, ny), avg);
+          copyPixel(processedImageData, get1dCoords(imageData, nx, ny), color);
         }
       }
     }
@@ -131,4 +136,70 @@ function getAverage(array) {
     sum += array[i];
   }
   return sum / array.length;
+}
+
+// Returns the color closest to the given starting color from the given palette.
+// If palette is empty, returns the starting color.
+function getClosestColor(color, palette) {
+  if (!palette.length) {
+    // The palette is empty. Return the starting color.
+    return color;
+  }
+  // Calculate distances of the color to all members of the palette.
+  var diffs = palette.map(function(paletteColor) {
+    return Math.abs(color[0] - paletteColor[0]) +
+      Math.abs(color[1] - paletteColor[1]) +
+      Math.abs(color[2] - paletteColor[2]) +
+      Math.abs(color[3] - paletteColor[3]);
+  });
+  var minI = 0;
+  var minDiff = diffs[0];
+  for (var i = 1; i < diffs.length; i++) {
+    if (diffs[i] < minDiff) {
+      minI = i;
+      minDiff = diffs[i];
+    }
+  }
+  return palette[minI];
+}
+
+//
+// Palette helpers
+//
+
+COLORPICKER = '<label class="palette-color"><input type="color"></label>';
+
+$(document).on('change', 'input[type=color]', function() {
+  this.parentNode.style.backgroundColor = this.value;
+});
+
+function addColor() {
+  $('#palette-colors').append(COLORPICKER);
+}
+
+function removeColor() {
+  if ($('.palette-color').length <= 1) {
+    // Only one color left, we don't want to remove that.
+    return;
+  }
+  $('#palette-colors').children().last().remove();
+}
+
+function loadImageWithPalette() {
+  var palette = [];
+  $('input[type=color]').each(function() {
+    palette.push(hexToRgba($(this).val()));
+  });
+  if (palette.length < 2) {
+    alert('Please add at least 2 colors to your palette.');
+    return;
+  }
+  loadImage(palette);
+}
+
+function hexToRgba(hex) {
+  return [parseInt(hex.slice(1, 3), 16),
+    parseInt(hex.slice(3, 5), 16),
+    parseInt(hex.slice(5, 7), 16),
+    255];
 }
